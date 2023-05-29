74f6d464a7912d63b1ff6ccd0c6a3c9628f73d39
anass Touati <anass.touati@hagergroup-external.com>
2023-05-29 18:02:52 +0200

    lazy log code imporvement

--- registration.php
+++ registration.php
@@ -5,9 +5,8 @@
  * Module AnassTouatiCoder_Base
  * Author Anass TOUATI anass1touati@gmail.com
  */
+require_once __DIR__ . '/src/Code.php';
 
 use Magento\Framework\Component\ComponentRegistrar;
-
-const AT_CODE =  \AT\Code::class;
 
 ComponentRegistrar::register(ComponentRegistrar::MODULE, 'AnassTouatiCoder_Base', __DIR__);
